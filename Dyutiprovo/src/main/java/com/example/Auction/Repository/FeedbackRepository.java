bebf68969ccfbffe8e9eca9cfbfffe53c6f90525
Dyutiprovo <sarkardyutiprovo@gmail.com>
2024-06-23 21:43:08 +0530

    removed faulty code

--- Dyutiprovo/src/main/java/com/example/Auction/Repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Auction.Repository;
-
-import com.example.Auction.Model.UserFeedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedbackRepository extends JpaRepository<UserFeedback, Integer> {
-}
